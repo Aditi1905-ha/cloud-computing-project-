ef2c829ce6d9e3d9e5df7a9c1600e5aab8747ffb
Aditi1905-ha <aditithegupta987@gmail.com>
2026-03-24 23:28:07 +0530

    Implement fetch request to send text input

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function send() {
     let text = document.getElementById("text").value;
-    console.log(text);
+
+    fetch("https://YOUR-NGROK-LINK/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text: text })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").innerHTML += 
+            "<p><b>You:</b> " + text + "</p>" +
+            "<p><b>Bot:</b> " + data.response + "</p>";
+    })
+    .catch(error => console.log(error));
 }
